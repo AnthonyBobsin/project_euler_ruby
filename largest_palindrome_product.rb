0bed0ac974ced16c0347f147aba553a9bee26042
AnthonyBobsin <bobsinj@gmail.com>
2015-06-03 21:26:28 -0400

    Implements functional is_palindrome? function

--- largest_palindrome_product.rb
+++ largest_palindrome_product.rb
@@ -1,3 +1,40 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+class String
+  def first
+    self[0]
+  end
+  def last
+    self[self.length - 1]
+  end
+  def cut_first_and_last
+    self[1...self.length - 1]
+  end
+end
+
+def is_palindrome? num
+  num_string = num.to_s
+  num_length = num_string.length
+  while (num_string)
+    first = num_string.first
+    last = num_string.last
+    if first.eql? last
+      num_string = num_string.cut_first_and_last
+    else
+      return false
+    end
+  end
+  true
+end
+
+@LARGEST_THREE_DIGIT_NUM = 999
+
+def one_number_reduce
+
+end
+
+def two_number_reduce
+
+end
+
