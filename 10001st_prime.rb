a7188c53e219f7d64da7e1474064178ba14ce5c4
AnthonyBobsin <bobsinj@gmail.com>
2015-06-17 20:46:21 -0400

    Starts is_prime function for problem 7

---
+++ 10001st_prime.rb
@@ -0,0 +1,16 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+
+prime_value = 10001
+
+def is_prime? num
+  is_prime = true
+  temp_num = 2
+  while test_num < num && is_prime
+    if num % test_num == 0
+      is_prime = false
+    end
+    temp_num += 1
+  end
+  is_prime
+end
